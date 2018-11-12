f75868645135140ffc1aebc7ede5a286689afe35
James Holland <jamoiholland@gmail.com>
2018-11-12 14:28:26 +0000

    push

--- test-main.tf
+++ test-main.tf
@@ -1,8 +1,3 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# BASIC TERRAFORM EXAMPLE
-# See test/terraform_aws_example.go for how to write automated tests for this code.
-# ---------------------------------------------------------------------------------------------------------------------
-
 data "template_file" "example" {
   template = "${var.example}"
 }
